d7b9aa7447cbaea61332fa9640bdd5c1e4cdce2e
3tyshm <152601383+3tyshm@users.noreply.github.com>
2023-12-03 17:03:49 +0330

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+float x, y, z, a, b, c, d, e, f, g, h;
+cout << "enter your number for x ";
+cin >> x;
+cout << "enter your number for y";
+cin >> y;
+cout << "enter your number for z";
+cin >> z;
+
+   a = x - y;
+   b = x + y;
+   c - x + y + z;
+   d = 1;
+   e = a * b;
+   f = c * c;
+   g = f + d;
+   h = e / g;
+   
+   cout << " the answer is    " << h;
+}
