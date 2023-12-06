7f9dba08acc1970976a076524a388fb542dac005
3tyshm <152601383+3tyshm@users.noreply.github.com>
2023-12-07 00:04:59 +0330

    HW 5 - 2

---
+++ main5-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+main( ) {
+ char a;
+ cout << "Enter a chractur.";
+ cin >> a;
+ 
+ switch (a){
+ 	case 'a':
+ 		cout << "VOWEL";
+ 		break;
+ 	case 'o':
+ 		cout << "VOWEL";
+ 		break;
+ 	case 'i':
+ 		cout << "VOWEL";
+ 	case 'u':
+ 		cout << "VOWEL";
+ 	case 'e':
+ 		cout << "VOWEL";
+ }
+}
