2ba2da21576095b7a6514fd1e9bc05c35aa6e9a1
3tyshm <152601383+3tyshm@users.noreply.github.com>
2024-01-16 13:06:09 +0330

    HW 7-1

---
+++ HW 7-1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+using namespace std
+int main() {
+
+int m,n;
+
+int s;
+
+cout<<"please enter your number:";
+cin>>m;
+cin>n;
+for (s= 1= 0; n > 0; i--) {
+{
+    s=s*m;
+}
+
+cout<<s;
+
+
+}
+
+
